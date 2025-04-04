1b7376ed2d0b94a665efbdde903e00d73eeced38
HAZARD DUTY PAY! <odfoytch@gmail.com>
2025-04-04 09:29:35 +0300

    started

---
+++ Lab1/Lab1Medv/Lab1Medv/Acoustic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guitar.Common
+{
+    internal class Acoustic : Guitar
+    {
+        public int PickupCount { get; set; }
+        public StringType StringType { get; set; }
+        public Acoustic(string name, int stringcount, int scalelength, float price, int pickupcount, StringType stringType) : base(name, stringcount, scalelength, price)
+        {
+            name = Name;
+            stringcount = StringCount;
+            scalelength = ScaleLength;
+            pickupcount = PickupCount;
+            stringType = StringType;
+            price = Price;
+        }
+    }
+    
+    enum StringType { Steel, Nylon}
+}
---
+++ Lab1/Lab1Medv/Lab1Medv/Guitar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guitar.Common
+{
+    public class Guitar
+    {
+        public Guid Id { get; set; }
+        public int ScaleLength { get; set; }
+        public string Name { get; set; }
+        public int StringCount { get; set; }
+        public float Price { get; set; }
+        public Guitar(string name, int stringcount, int scalelength, float price)
+        {
+            name = Name;
+            stringcount = StringCount;
+            scalelength = ScaleLength;
+            price = Price;
+        }
+    }
+}
