f25154c34020fc0774e3183d0c236b5ccf99e2b3
HAZARD DUTY PAY! <odfoytch@gmail.com>
2025-05-05 16:12:35 +0300

    start

---
+++ Lab1/Lab1Medv/Guitar.Infrastructure/Models/GuitarModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guitar.Infrastructure.Models
+{
+    public class GuitarModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int StringCount { get; set; }
+        public int ScaleLength { get; set; }
+        public float Price { get; set; }
+
+        // Foreign key to Player
+        public Guid PlayerId { get; set; }
+        public PlayerModel Player { get; set; }
+    }
+}
---
+++ Lab1/Lab1Medv/Guitar.Infrastructure/Models/PlayerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guitar.Infrastructure.Models
+{
+    public class PlayerModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int YearsExperience { get; set; }
+
+        // Зв’язок один-до-багатьох: один Player -> багато GuitarModel
+        public ICollection<GuitarModel> Guitars { get; set; } = new List<GuitarModel>();
+    }
+}
